7766bdb5f7e0251e22c6cb8f09b729189f6d2efd
PedroPortoHx <tlayder667@gmail.com>
2024-11-25 00:20:56 -0300

    metodo pra cadastrar usuario
    STC

--- Springboot/cuidapcd/src/main/java/com/poo/cuidapcd/conexao/MySQLConnectionController.java
+++ Springboot/cuidapcd/src/main/java/com/poo/cuidapcd/conexao/MySQLConnectionController.java
@@ -9,7 +9,12 @@ import java.sql.SQLException;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.poo.cuidapcd.entity.Cliente;
+import com.poo.cuidapcd.entity.Profissional;
 
 //Isso aqui vai virar o DAO, so é o controller por enquanto pra testar se ta funcionando a conexão de um modo mais rapido
 @Controller
@@ -73,5 +78,75 @@ public class MySQLConnectionController {
             e.printStackTrace();
         } 
         return autenticado;
+    }
+
+    @PostMapping("/enviarFormulario")
+    public void receberFormularioProfissional(@ModelAttribute Profissional profissional) {
+
+        String sqlUsuario = "INSERT INTO usuario (nome, email, senha, telefone, cpf, endereco) VALUES (?, ?, ?, ?, ?)";
+        String sqlProf = "INSERT INTO profissional (id, formacao, experiencia, regiao_de_atendimento, sobre, cnpj, registro_profissional, arquivo_curriculo, arquivo_certificado, arquivo_foto, cpf) VALUES ((SELECT id FROM usuario WHERE cpf = ?), ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+
+        try (Connection connection = DriverManager.getConnection(dbUrl, dbUsername, dbPassword);
+             PreparedStatement preparedStatement = connection.prepareStatement(sqlUsuario)) {
+            preparedStatement.setString(1, profissional.getNome());
+            preparedStatement.setString(2, profissional.getEmail());
+            preparedStatement.setString(3, profissional.getSenha());
+            preparedStatement.setString(4, profissional.getTelefone());
+            preparedStatement.setString(5, profissional.getCpf());
+            preparedStatement.setString(6, profissional.getEndereco());
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } 
+
+        try (Connection connection = DriverManager.getConnection(dbUrl, dbUsername, dbPassword);
+             PreparedStatement preparedStatement = connection.prepareStatement(sqlProf)) {
+                preparedStatement.setString(1, profissional.getCpf());
+                preparedStatement.setString(2, profissional.getFormacao());
+                preparedStatement.setString(3, profissional.getExperiencia());
+                preparedStatement.setString(4, profissional.getRegiaoDeAtendimento());
+                preparedStatement.setString(5, profissional.getSobre());
+                preparedStatement.setString(6, profissional.getCnpj());
+                preparedStatement.setString(7, profissional.getRegistroProfissional());
+                preparedStatement.setString(8, profissional.getArquivoCurriculo());
+                preparedStatement.setString(9, profissional.getArquivoCertificado());
+                preparedStatement.setString(10, profissional.getArquivoFoto());
+                preparedStatement.setString(11, profissional.getCpf());
+                preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } 
+        
+    }
+
+    @PostMapping("/enviarFormulario")
+    public void receberFormularioCliente(@ModelAttribute Cliente cliente) {
+
+        String sqlUsuario = "INSERT INTO usuario (nome, email, senha, telefone, cpf, endereco) VALUES (?, ?, ?, ?, ?)";
+        String sqlCliente = "INSERT INTO cliente (id, cpf, preferencias) VALUES ((SELECT id FROM usuario WHERE cpf = ?), ?, ?)";
+
+        try (Connection connection = DriverManager.getConnection(dbUrl, dbUsername, dbPassword);
+             PreparedStatement preparedStatement = connection.prepareStatement(sqlUsuario)) {
+            preparedStatement.setString(1, cliente.getNome());
+            preparedStatement.setString(2, cliente.getEmail());
+            preparedStatement.setString(3, cliente.getSenha());
+            preparedStatement.setString(4, cliente.getTelefone());
+            preparedStatement.setString(5, cliente.getCpf());
+            preparedStatement.setString(6, cliente.getEndereco());
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } 
+
+        try (Connection connection = DriverManager.getConnection(dbUrl, dbUsername, dbPassword);
+             PreparedStatement preparedStatement = connection.prepareStatement(sqlCliente)) {
+                preparedStatement.setString(1, cliente.getCpf());
+                preparedStatement.setString(2, cliente.getCpf());
+                preparedStatement.setString(3, cliente.getPreferencias());
+                preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } 
+        
     }
 }
--- Springboot/cuidapcd/src/main/java/com/poo/cuidapcd/entity/Profissional.java
+++ Springboot/cuidapcd/src/main/java/com/poo/cuidapcd/entity/Profissional.java
@@ -1,5 +1,84 @@
 package com.poo.cuidapcd.entity;
 
-public class Profissional {
 
+public class Profissional extends Usuario{
+    private String formacao;
+    private String experiencia;
+    private String regiaoDeAtendimento;
+    private String sobre;
+    private String cnpj;
+    private String registroProfissional;
+    private String arquivoCurriculo;
+    private String arquivoCertificado;
+    private String arquivoFoto;
+
+    
+    public Profissional(Long id, String nome, String email, String senha, String telefone, String cpf, String endereco,
+            String formacao, String experiencia, String regiaoDeAtendimento, String sobre, String cnpj,
+            String registroProfissional, String arquivoCurriculo, String arquivoCertificado, String arquivoFoto) {
+        super(id, nome, email, senha, telefone, cpf, endereco);
+        this.formacao = formacao;
+        this.experiencia = experiencia;
+        this.regiaoDeAtendimento = regiaoDeAtendimento;
+        this.sobre = sobre;
+        this.cnpj = cnpj;
+        this.registroProfissional = registroProfissional;
+        this.arquivoCurriculo = arquivoCurriculo;
+        this.arquivoCertificado = arquivoCertificado;
+        this.arquivoFoto = arquivoFoto;
+    }
+    public String getFormacao() {
+        return formacao;
+    }
+    public void setFormacao(String formacao) {
+        this.formacao = formacao;
+    }
+    public String getExperiencia() {
+        return experiencia;
+    }
+    public void setExperiencia(String experiencia) {
+        this.experiencia = experiencia;
+    }
+    public String getRegiaoDeAtendimento() {
+        return regiaoDeAtendimento;
+    }
+    public void setRegiaoDeAtendimento(String regiaoDeAtendimento) {
+        this.regiaoDeAtendimento = regiaoDeAtendimento;
+    }
+    public String getSobre() {
+        return sobre;
+    }
+    public void setSobre(String sobre) {
+        this.sobre = sobre;
+    }
+    public String getCnpj() {
+        return cnpj;
+    }
+    public void setCnpj(String cnpj) {
+        this.cnpj = cnpj;
+    }
+    public String getRegistroProfissional() {
+        return registroProfissional;
+    }
+    public void setRegistroProfissional(String registroProfissional) {
+        this.registroProfissional = registroProfissional;
+    }
+    public String getArquivoCurriculo() {
+        return arquivoCurriculo;
+    }
+    public void setArquivoCurriculo(String arquivoCurriculo) {
+        this.arquivoCurriculo = arquivoCurriculo;
+    }
+    public String getArquivoCertificado() {
+        return arquivoCertificado;
+    }
+    public void setArquivoCertificado(String arquivoCertificado) {
+        this.arquivoCertificado = arquivoCertificado;
+    }
+    public String getArquivoFoto() {
+        return arquivoFoto;
+    }
+    public void setArquivoFoto(String arquivoFoto) {
+        this.arquivoFoto = arquivoFoto;
+    }
 }
