7b36cf35e4f7830ed127bb125c58630c004b8118
PedroPortoHx <tlayder667@gmail.com>
2024-11-26 22:25:45 -0300

    Login-verificação

--- Springboot/cuidapcd/src/main/java/com/poo/cuidapcd/controller/LoginController.java
+++ Springboot/cuidapcd/src/main/java/com/poo/cuidapcd/controller/LoginController.java
@@ -1,6 +1,7 @@
-package com.example.demo.controller;
+package com.poo.cuidapcd.controller;
 
-import com.example.demo.service.UsuarioService;
+
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,11 +9,12 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+
 @Controller
 public class LoginController {
 
     @Autowired
-    private UsuarioService usuarioService;
+    private MySQLConnectionController usuarioService;
 
     // Exibe a tela de login
     @GetMapping("/login")
@@ -21,7 +23,7 @@ public class LoginController {
     }
 
     // Processa o login
-    @PostMapping("/login")
+    @PostMapping("/loginAutenticar")
     public String autenticarLogin(@RequestParam("email") String email,
                                    @RequestParam("senha") String senha,
                                    RedirectAttributes redirectAttributes) {
@@ -31,11 +33,16 @@ public class LoginController {
         // Se o login for bem-sucedido
         if (autenticado) {
             // Redireciona para a página principal ou para a página desejada
-            return "redirect:/home";  // Ou outra página que você queira redirecionar após login
+            return "redirect:/index";  // Ou outra página que você queira redirecionar após login
         } else {
             // Se o login falhar, exibe uma mensagem de erro
             redirectAttributes.addFlashAttribute("erro", "Usuário ou senha inválidos.");
             return "redirect:/login";  // Retorna para a página de login
         }
+    }
+
+    @GetMapping("/index")
+    public String logado() {
+        return "index";  // Aqui você pode retornar o nome da sua página HTML de login
     }
 }
