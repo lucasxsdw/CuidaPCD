e2d4316fdfbd594e72cce8020420588096910ac0
SanchoArruda <gucairesarruda@gmail.com>
2024-12-03 11:45:29 -0300

    Delete

--- BD/CuidaPCD-BD.sql
+++ BD/CuidaPCD-BD.sql
@@ -1,5 +1,111 @@
-
+-- BD 2.0
 drop database if exists CuidaPCD;
+create database CuidaPCD;
+use CuidaPCD;
+
+create table Usuario (
+    id int auto_increment,
+    nome TEXT,
+    email varchar(255) Unique,
+    senha TEXT,
+    telefone varchar(15) Unique,
+    cpf varchar(15) Unique,
+    primary key(id)
+);
+
+create table Cliente(
+    id int,
+    preferencias TEXT,
+    arquivoFoto varchar(500),
+    primary key (id),
+    FOREIGN KEY (id) REFERENCES Usuario(id) ON DELETE CASCADE
+);
+
+create table Especialidade(
+    nome varchar(100),
+    descricao varchar(100),
+    primary key(nome, descricao)
+);
+
+create table Profissional(
+    formacao TEXT,
+    experiencia TEXT,
+    sobre TEXT,
+    id int,
+    cnpj varchar(20) Unique,
+    registroProfissional varchar(30) Unique,
+    arquivoCurriculo varchar(500),
+    arquivoFoto varchar(500),
+    arquivoCertificado varchar(500),
+    especialidade varchar(100),
+    especialidadeDescricao varchar(100),
+    primary key (id),
+    FOREIGN KEY (id) REFERENCES Usuario(id) ON DELETE CASCADE,
+    FOREIGN KEY (especialidade, especialidadeDescricao) REFERENCES Especialidade(nome, descricao)
+);
+
+create table Avaliacao(
+    idCliente int,
+    idProfissional int,
+    nota int,
+    comentario TEXT,
+    dataAvaliacao datetime,
+    primary key (idCliente, idProfissional),
+    FOREIGN KEY (idProfissional) REFERENCES Profissional(id) ON DELETE CASCADE,
+    FOREIGN KEY (idCliente) REFERENCES Cliente(id) ON DELETE CASCADE
+);
+
+create table Endereco(
+    id int,
+    rua TEXT,
+    numero TEXT,
+    bairro TEXT,
+    cidade TEXT,
+    estado TEXT,
+    cep TEXT,
+    primary key(id),
+    FOREIGN KEY (id) REFERENCES Profissional(id) ON DELETE CASCADE
+);
+
+INSERT INTO Especialidade (nome, descricao) VALUES
+('PSICOLOGIA_CLINICA', 'Psicologia Clínica'),
+('PSICOLOGIA_INFANTIL', 'Psicologia Infantil'),
+('NEUROPSICOLOGIA', 'Neuropsicologia'),
+('PSICOLOGIA_ORGANIZACIONAL', 'Psicologia Organizacional'),
+('PSICOTERAPIA_COGNITIVA', 'Psicoterapia Cognitiva-Comportamental'),
+('PSICOLOGIA_ESCOLAR', 'Psicologia Escolar'),
+
+-- Medicina
+('CARDIOLOGIA', 'Cardiologia'),
+('NEUROLOGIA', 'Neurologia'),
+('PEDIATRIA', 'Pediatria'),
+('GERIATRIA', 'Geriatria'),
+('DERMATOLOGIA', 'Dermatologia'),
+
+-- Fisioterapia
+('FISIOTERAPIA_DESPORTIVA', 'Fisioterapia Desportiva'),
+('FISIOTERAPIA_RESPIRATORIA', 'Fisioterapia Respiratória'),
+('FISIOTERAPIA_NEUROFUNCIONAL', 'Fisioterapia Neurofuncional'),
+('FISIOTERAPIA_PEDIATRICA', 'Fisioterapia Pediátrica'),
+('FISIOTERAPIA_GERIATRICA', 'Fisioterapia Geriátrica'),
+
+-- Nutrição
+('NUTRICAO_CLINICA', 'Nutrição Clínica'),
+('NUTRICAO_ESPORTIVA', 'Nutrição Esportiva'),
+('NUTRICAO_MATERNO_INFANTIL', 'Nutrição Materno-Infantil'),
+('NUTRICAO_VEGETARIANA', 'Nutrição Vegetariana'),
+('NUTRICAO_ONCOLOGICA', 'Nutrição Oncológica'),
+
+-- Odontologia
+('ODONTOPEDIATRIA', 'Odontopediatria'),
+('ORTODONTIA', 'Ortodontia'),
+('IMPLANTODONTIA', 'Implantodontia'),
+('PERIODONTIA', 'Periodontia'),
+('ODONTOLOGIA_ESTETICA', 'Odontologia Estética');
+
+
+-- BD 1.0
+/*drop database if exists CuidaPCD;
 create database CuidaPCD;
 use CuidaPCD;
 
@@ -99,8 +205,9 @@ mensagem TEXT,
 tipoNotificacao TEXT,
 dataHora datetime,
 FOREIGN KEY (id) REFERENCES Usuario(id)
-);
+);/*
 
+-- BD antigo1
 /*drop database if exists CuidaPCD;
 create database CuidaPCD;
 use CuidaPCD;
